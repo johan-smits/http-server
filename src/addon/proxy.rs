682cba1fe2679999f4cfeecf184c740e25949a11
Esteban Borai <estebanborai@gmail.com>
2021-12-23 20:05:33 -0300

    feat(proxy): remove hop-by-hop http headers

--- src/addon/proxy.rs
+++ src/addon/proxy.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use http::header::{HeaderName, HeaderValue};
 use hyper::Body;
 
@@ -19,7 +21,9 @@ impl Proxy {
     ///
     /// Via: [ <protocol-name> "/" ] <protocol-version> <host> [ ":" <port> ]
     ///
-    /// Refer: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Via
+    /// ## References
+    ///
+    /// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Via
     async fn add_via_header(&self, request: Request<Body>) {
         let mut request = request.lock().await;
         let via_header_str = format!("{:?} Rust http-server", request.version());
@@ -50,10 +54,51 @@ impl Proxy {
             .headers_mut()
             .append(HeaderName::from_static("via"), via_header);
     }
+
+    /// Removes Hop-by-Hop headers which are only meaningful for a singles
+    /// transport-level connection and should not be stored by caches following
+    /// RFC2616.
+    ///
+    /// The following HTTP/1.1 headers are hop-by-hop headers:
+    ///
+    ///   - Connection
+    ///   - Keep-Alive
+    ///   - Proxy-Authenticate
+    ///   - Proxy-Authorization
+    ///   - TE
+    ///   - Trailers
+    ///   - Transfer-Encoding
+    ///   - Upgrade
+    ///
+    /// ## References
+    ///
+    /// http://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html
+    async fn remove_hbh_headers(&self, request: Request<Body>) {
+        use http::header::{
+            CONNECTION, PROXY_AUTHENTICATE, PROXY_AUTHORIZATION, TE, TRAILER, TRANSFER_ENCODING,
+            UPGRADE,
+        };
+
+        let mut request = request.lock().await;
+        let headers = request.headers_mut();
+
+        headers.remove(CONNECTION);
+        headers.remove(HeaderName::from_static("keep-alive"));
+        headers.remove(PROXY_AUTHENTICATE);
+        headers.remove(PROXY_AUTHORIZATION);
+        headers.remove(TE);
+        headers.remove(TRAILER);
+        headers.remove(TRANSFER_ENCODING);
+        headers.remove(UPGRADE);
+    }
 }
 
 mod tests {
     use http::header::{HeaderName, HeaderValue};
+    use http::header::{
+        CONNECTION, PROXY_AUTHENTICATE, PROXY_AUTHORIZATION, TE, TRAILER, TRANSFER_ENCODING,
+        UPGRADE,
+    };
     use hyper::Body;
     use std::sync::Arc;
     use tokio::sync::Mutex;
@@ -108,5 +153,45 @@ mod tests {
             via_header_value,
             "HTTP/1.1 GoodProxy, HTTP/1.1 Rust http-server"
         );
+    }
+
+    #[tokio::test]
+    async fn removes_hbh_headers() {
+        let proxy = Proxy::new();
+        let mut request = http::Request::new(Body::empty());
+        let headers = request.headers_mut();
+        let headers_to_add = vec![
+            (CONNECTION, HeaderValue::from_str("keep-alive").unwrap()),
+            (
+                PROXY_AUTHENTICATE,
+                HeaderValue::from_static(r#"Basic realm="Access to the internal site""#),
+            ),
+            (
+                PROXY_AUTHORIZATION,
+                HeaderValue::from_static("Basic YWxhZGRpbjpvcGVuc2VzYW1l"),
+            ),
+            (TE, HeaderValue::from_static("compress")),
+            (TRAILER, HeaderValue::from_static("Expires")),
+            (TRANSFER_ENCODING, HeaderValue::from_static("chunked")),
+            (UPGRADE, HeaderValue::from_static("example/1, foo/2")),
+        ];
+
+        for (name, value) in headers_to_add {
+            headers.append(name, value);
+        }
+
+        let request = Arc::new(Mutex::new(request));
+
+        proxy.remove_hbh_headers(Arc::clone(&request)).await;
+
+        let request = request.lock().await;
+
+        assert!(!request.headers().contains_key(CONNECTION));
+        assert!(!request.headers().contains_key(PROXY_AUTHENTICATE));
+        assert!(!request.headers().contains_key(PROXY_AUTHORIZATION));
+        assert!(!request.headers().contains_key(TE));
+        assert!(!request.headers().contains_key(TRAILER));
+        assert!(!request.headers().contains_key(TRANSFER_ENCODING));
+        assert!(!request.headers().contains_key(UPGRADE));
     }
 }
